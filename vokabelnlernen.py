4862caf6d7e6a73d8fcd4dbbd3defa2cdd150948
tenklo <57324024+tenklo@users.noreply.github.com>
2019-11-19 12:45:49 +0100

    Added another exit option next to ctrl+c

--- vokabelnlernen.py
+++ vokabelnlernen.py
@@ -32,6 +32,9 @@ def main():
         if guess == w:
             print ("Richtig: "+ vok + " = "+ w)
             counter+=1
+        elif guess == "e":
+            whenDone()
+            break
         elif guess=="":
             print("Falsch. Korrekt ist: "+vok+" = " + w) 
             wrong.append(vok)
